c302c56ea775f7171c82d12611e28d6dea2c6ba5
Nathan Mische <nmische@gmail.com>
2013-01-19 18:44:43 -0500

    Bumping version number.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "nmische@gmail.com"
 license          "Apache 2.0"
 description      "Sets up ColdFire development environment."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 supports "ubuntu"
 
