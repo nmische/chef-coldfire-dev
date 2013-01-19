c577ae04206d6f79d04e3273209fc8b9bc9458de
Nathan Mische <nmische@gmail.com>
2013-01-19 17:46:46 -0500

    Changing quotes in metadata.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Sets up ColdFire development environment."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-supports 'ubuntu'
+supports "ubuntu"
 
 depends "ant"
 depends "coldfusion10"
